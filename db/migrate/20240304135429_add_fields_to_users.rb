bd75850410199ffe86fa8bfbd0eef787d270e8d0
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-03-04 14:55:42 +0100

    model user corrigé

--- db/migrate/20240304135429_add_fields_to_users.rb
+++ db/migrate/20240304135429_add_fields_to_users.rb
@@ -1,5 +1,9 @@
 class AddFieldsToUsers < ActiveRecord::Migration[7.1]
   def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :phone_number, :string
     add_column :users, :role, :string
     add_column :users, :skill, :string
     add_column :users, :siret_number, :string
