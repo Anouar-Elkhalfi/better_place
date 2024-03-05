c540fe00e109db83a892abdee72cd5d9f2623773
Bruno de Frias <brunodf@hotmail.fr>
2024-03-05 06:33:08 +0100

    generation projects_controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :users, only: [:show, :new] do
+   resources :projects, only: [:show, :create, :new]
+  end
+
+  resources :projects, only: [:new]
+
 end
