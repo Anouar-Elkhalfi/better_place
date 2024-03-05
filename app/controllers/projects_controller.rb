66207aebab51fce0923e68669d315949029e0b19
Pedro GOMES PINTO <pedrogomespinto@gmail.com>
2024-03-05 23:01:55 +0100

    routes update project creation almost done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,26 @@
 class ProjectsController < ApplicationController
-  # def new
-  #   @project = Project.new
-  # end
+  def new
+    @project = Project.new
+    @works = Work.all
+    authorize @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project = ProjectWork.new(project: @project)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def artisans
+
+  end
+  private
+  def project_params
+    params.require(:project).permit(:title, :description, :location, :budget, :start_date, work_ids: [])
+   end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :project_works
-  has_many :works, through: :project_work
+  has_many :works, through: :project_works
   has_many :project_requests
   # has_many :reviews, through: :project_request
 end
