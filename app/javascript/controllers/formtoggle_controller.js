27c844202d566a702839c240f26bc445d0eca4ec
Wilfried Fosse <wilfriedfosse2@gmail.com>
2024-03-08 15:21:13 +0100

    done

--- app/javascript/controllers/formtoggle_controller.js
+++ app/javascript/controllers/formtoggle_controller.js
@@ -2,22 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="formtoggle"
 export default class extends Controller {
-  static targets = ["page1", "page2", "form"];
+  static targets = ["page1", "page2", "form", "progress"];
 
   connect() {
     this.showPage(1);
   }
   showPage(page){
     if (page === 1) {
+      this.progressTarget.style.width = '33%'
       this.page1Target.classList.remove('d-none')
     } else {
       this.page1Target.classList.add('d-none')
     }
+
     if (page === 2) {
+      this.progressTarget.style.width = '66%'
       this.page2Target.classList.remove('d-none')
     } else {
       this.page2Target.classList.add('d-none')
-    } 
+    }
   }
 
   nextPage(){
