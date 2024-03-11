44dcd08974156cf11dda2faaf1b7a6e16022c7eb
Bruno de Frias <brunodf@hotmail.fr>
2024-03-11 17:28:22 +0100

    artisans selection front

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_150154) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_153134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_07_150154) do
     t.integer "siret_number"
     t.string "company_name"
     t.string "image"
+    t.string "benefit"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
