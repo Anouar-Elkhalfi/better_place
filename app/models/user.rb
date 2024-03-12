30134fd0d9d3497c133a18d6bec7ef4ad8e58b30
Bruno de Frias <brunodf@hotmail.fr>
2024-03-12 16:57:17 +0100

    correction fornt modal et artisans list

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :projects
+  has_many :projects, dependent: :destroy
   has_many :estimates
   has_many :reviews
   has_many :messages, dependent: :destroy
